cf6e7813aaaf14855d4f769868ecc5236a343c03
Erwin Rooijakkers <erwin.rooijakkers@alliander.com>
2017-07-02 17:37:46 +0200

    feat: compile Solidity script

--- compile-solidity.sh
+++ compile-solidity.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
 cd resources/public/contracts/src
+
+for fileName in *; do
+    echo $fileName
+    solc --overwrite --optimize --bin --abi $fileName -o ../build/
+done
+
+cd ../build
+for fileName in *; do
+    wc -c $fileName | awk -v name=$fileName '{print name ": " $1 " bytes"}'
+done
