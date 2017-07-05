b6e0a0b0e040de60874097da9e1175c086e6251c
Erwin Rooijakkers <erwin.rooijakkers@alliander.com>
2017-07-05 22:57:35 +0200

    chore: add comment to max-gas-limit

--- src/cljs/fleet/blockchain/constants.cljs
+++ src/cljs/fleet/blockchain/constants.cljs
@@ -1,5 +1,8 @@
 (ns fleet.blockchain.constants)
 
-(def max-gas-limit 4000000)
+(def max-gas-limit
+  "The maximum number of computational steps the transaction execution is
+  allowed to take"
+  4000000)
 
 (def contracts-version "0.1.0")
