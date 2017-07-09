87dda6ecea1bf454d18307d222b1ac53e4d6f984
Erwin Rooijakkers <erwin.rooijakkers@alliander.com>
2017-07-09 20:06:54 +0200

    refactor: remove unnecessary use of fns

--- src/cljs/fleet/views.cljs
+++ src/cljs/fleet/views.cljs
@@ -7,12 +7,11 @@
             [reagent.ratom :refer-macros [reaction]]))
 
 (defn logo []
-  (fn []
-    [:p#logo
-     [:a
-      {:href "/" :title "FlΞΞt"}
-      [:span.fa.fa-handshake-o]
-      [:span.text "Fleet"]]]))
+  [:p#logo
+   [:a
+    {:href "/" :title "FlΞΞt"}
+    [:span.fa.fa-handshake-o]
+    [:span.text "Fleet"]]])
 
 (def use-asset-handler
   (fn [e]
@@ -22,58 +21,52 @@
         (asset-manager/use-asset asset-name)))))
 
 (defn menu-panel []
-  (fn []
-    [:div#menucont.bodycontainer.clearfix
-     [:div.menutitle [:p
-                      [:span.fa.fa-reorder]
-                      [:strong "Menu"]]]
-     [:ul.menu
-      [:li.active [:a.active {:href "/" :title "Smart assets"}
-                   "Smart assets"]]
-      [:li [:a {:href "/"
-                :title "Use asset"
-                :onClick use-asset-handler}
-            "Use asset"]]]]))
+  [:div#menucont.bodycontainer.clearfix
+   [:div.menutitle [:p
+                    [:span.fa.fa-reorder]
+                    [:strong "Menu"]]]
+   [:ul.menu
+    [:li.active [:a.active {:href "/" :title "Smart assets"}
+                 "Smart assets"]]
+    [:li [:a {:href "/"
+              :title "Use asset"
+              :onClick use-asset-handler}
+          "Use asset"]]]])
 
 (defn social-media []
-  (fn []
-    [:div#socialmedia.clearfix
-     [:ul
-      [:li
-       [:a
-        {:rel   "external"
-         :href  "https://github.com/erooijak/fleet"
-         :title "Github"}
-        [:span.fa.fa-github]]]]]))
+  [:div#socialmedia.clearfix
+   [:ul
+    [:li
+     [:a
+      {:rel   "external"
+       :href  "https://github.com/erooijak/fleet"
+       :title "Github"}
+      [:span.fa.fa-github]]]]])
 
 (defn footer []
-  (fn []
-    [:div#footercont.clearfix
-     [:hr]
-     [:p
-      "Copyright © Fleet | Powered by "
-      [:a {:rel "external" :href "https://ethereum.org"} "Ethereum"]]]))
+  [:div#footercont.clearfix
+   [:hr]
+   [:p
+    "Copyright © Fleet | Powered by "
+    [:a {:rel "external" :href "https://ethereum.org"} "Ethereum"]]])
 
 (defn page-header []
-  (fn []
-    [:div#page-header
-     [:h1 "Smart Asset Management"]]))
+  [:div#page-header
+   [:h1 "Smart Asset Management"]])
 
 (defn explanation []
-  (fn []
-    [:div "Add the addresses of the beneficiaries involved in the smart asset
+  [:div "Add the addresses of the beneficiaries involved in the smart asset
  below. Use the slider to set the relative weight of the usage price the
  beneficiary receives on usage of the asset. Then deploy the smart asset
  contract to the Ethereum blockchain. When the asset is used, the price for
  usage will be deducted from the consumer, and the added beneficiaries involved
  will be paid the relative weight (their weight as percentage of total
- weight)."]))
+ weight)."])
 
 (defn slider [value min max]
-  (fn []
-    [:input {:type "range" :value @value :min min :max max
-             :on-change (fn [e]
-                          (reset! value (.-target.value e)))}]))
+  [:input {:type "range" :value @value :min min :max max
+           :on-change (fn [e]
+                        (reset! value (.-target.value e)))}])
 
 (defn add-beneficiary []
   (let [address (r/atom "")
@@ -154,19 +147,18 @@
         "Publish on the blockchain"]])))
 
 (defn main-panel []
-  (fn []
-    [:div
-     [:div#left
-      [logo]
-      [menu-panel]
-      [social-media]]
-     [:div#right.clearfix
-      [:div#custom-page
-       [page-header]
-       [explanation]
-       [add-beneficiary]
-       [:br]
-       [beneficiaries]
-       [:br]
-       [add-smart-asset]
-       [footer]]]]))
+  [:div
+   [:div#left
+    [logo]
+    [menu-panel]
+    [social-media]]
+   [:div#right.clearfix
+    [:div#custom-page
+     [page-header]
+     [explanation]
+     [add-beneficiary]
+     [:br]
+     [beneficiaries]
+     [:br]
+     [add-smart-asset]
+     [footer]]]])
